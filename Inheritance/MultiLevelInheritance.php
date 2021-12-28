89016a01ed1459f566459a7924d0c22b0c8dc1ad
“ArafathvBaig” <arafathbaig1997@gmail.com>
2021-12-29 00:08:06 +0530

    Added: All OOPS Concept Files to Master

---
+++ Inheritance/MultiLevelInheritance.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Classes to Compute Multi level inheritance Problem
+ */
+class MyName
+{
+    /**
+     * Function to print name
+     * passing name as parameter
+     */
+    public function myName($name)
+    {
+        echo "Name:: " . $name;
+    }
+}
+class MyAge extends MyName
+{
+    /**
+     * Function to print age
+     * passing age as parameter
+     */
+    public function myAge($age)
+    {
+        echo "\nAge:: " . $age;
+    }
+}
+class MyNumber extends MyAge
+{
+    /**
+     * Function to print number
+     * passing number as parameter
+     */
+    public function myNumber($number)
+    {
+        echo "\nPhone Number:: " . $number;
+    }
+}
+$myNumberObj = new MyNumber();
+
+$name = readline('Enter Your Name: ');
+$age = readline('Enter Your Age: ');
+$number = readline('Enter Your Phone Number: ');
+$myNumberObj->myName($name);
+$myNumberObj->myAge($age);
+$myNumberObj->myNumber($number);
---
+++ Interface/Interface.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Interfaces of different functions to be
+ * implemented into MyDetails class and 
+ * get there their implementation
+ */
+interface MyName
+{
+    public function myName($name);
+}
+interface MyAge extends MyName
+{
+    public function myAge($age);
+}
+interface MyNumber extends myAge
+{
+    public function myNumber($number);
+}
+
+/**
+ * Author -> Arafath Baig
+ * PHP Version -> 8.0.9
+ * Class to Compute Interface Problem
+ */
+class MyDetails implements MyNumber
+{
+    public function myName($name)
+    {
+        echo "Name:: " . $name;
+    }
+    public function myAge($age)
+    {
+        echo "\nAge:: " . $age;
+    }
+    public function myNumber($number)
+    {
+        echo "\nNumber:: " . $number;
+    }
+}
+
+$myDetatils = new MyDetails();
+
+$name = readline('Enter Your Name: ');
+$age = readline('Enter Your Age: ');
+$number = readline('Enter Your Phone Number: ');
+
+$myDetatils->myName($name);
+$myDetatils->myAge($age);
+$myDetatils->myNumber($number);
